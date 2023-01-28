6c41eea3b28c0e7cfb7b390439a397ec0ede003f
Karel Kvítek <karel.kvitek@seznam.cz>
2023-01-28 11:29:20 +0100

    Next font added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+// Next.js - Google fonts
+import { Poppins } from "@next/font/google"
+
+const poppins = Poppins({ weight: ["400", "500", "600"], subsets: ["latin"] })
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`app ${poppins.className}`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
