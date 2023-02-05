c84ddf7c9c53993c99a61bf962caaa5c1cc784ae
Karel Kvítek <karel.kvitek@seznam.cz>
2023-02-05 16:00:40 +0100

    Smooth scroll

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,15 +1,12 @@
-// Next.js
-import Link from "next/link"
-
 const Button = ({ children, onClick, href }) => {
   const classNames =
     "min-w-[120px] rounded-[20px] border-2 border-sky-600 bg-white py-2 px-8 text-sky-600 hover:border-orange-500 hover:bg-orange-500 hover:text-white"
 
   if (href) {
     return (
-      <Link href={href}>
-        <button className={classNames}>{children}</button>
-      </Link>
+      <a href={href} className={classNames}>
+        {children}
+      </a>
     )
   }
   return (
--- components/NavList/NavList.js
+++ components/NavList/NavList.js
@@ -1,24 +1,21 @@
-// Next.js
-import Link from "next/link"
-
 const NavList = () => {
   return (
     <ul className="flex items-center gap-x-8.5">
       <li>
-        <Link
+        <a
           className="text-lg text-sky-600 hover:text-orange-500"
           href="#jak-na-to"
         >
           Jak na to?
-        </Link>
+        </a>
       </li>
       <li>
-        <Link
+        <a
           className="text-lg text-sky-600 hover:text-orange-500"
           href="#co-je-to-qr-kod"
         >
           Co je to QR kód?
-        </Link>
+        </a>
       </li>
     </ul>
   )
