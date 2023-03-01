3dc48116cbd75146411d6a1cd25335f22f24ba78
Karel Kvítek <karel.kvitek@seznam.cz>
2023-03-01 09:00:48 +0100

    Packages upgraded & @next/font removed

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@
 import "../styles/global.css"
 
 // Next.js - Google fonts
-import { Poppins } from "@next/font/google"
+import { Poppins } from "next/font/google"
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600"],
