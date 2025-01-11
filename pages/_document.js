f8ab6fa9a9714bf89b8a30f8a3566379cfcd58cd
Karlos137 <karel.kvitek@seznam.cz>
2025-01-11 07:51:54 +0100

    add gloogle ads script

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,11 @@ export default function Document() {
           type="text/css"
           dangerouslySetInnerHTML={{ __html: mediaStyles }}
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9782317089565725"
+          crossOrigin="anonymous"
+        ></script>
         <Favicon />
       </Head>
       <body>
